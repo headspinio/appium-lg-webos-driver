036aeb1db82c598779ec123be5f444d304d3ba24
Jonathan Lipps <jlipps@gmail.com>
2022-05-27 14:50:58 -0700

    chore: set up test harness

---
+++ test/unit/driver.spec.js
@@ -0,0 +1,7 @@
+import { WebOSDriver } from '../../lib';
+
+describe('WebOSDriver', function() {
+  it('should be importable and instantiable', function() {
+    should.exist(new WebOSDriver());
+  })
+})
